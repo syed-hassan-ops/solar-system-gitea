0cd925d9b327fd4cdae7b8be7256b0706fb89201
syed-hassan-ops <syed.hassan@techtreeit.com>
2024-12-17 15:50:49 +0530

    Update app.js
    App.js credentials masked

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+mongoose.connect('mongodb://localhost:27017/test', {
+    user: 'root',
+    pass: 'example',
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
