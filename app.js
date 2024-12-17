6acdfd179b1feee9af5040c8c755a86cad9fb134
syed-hassan-ops <syed.hassan@techtreeit.com>
2024-12-17 17:35:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,16 +13,21 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect('mongodb://localhost:27017/test', {
+// const mongoose = require('mongoose');
+
+// MongoDB URI for local connection (without username and password)
+const dbUri = 'mongodb://localhost:27017/test';  // Replace 'test' with your database name
+
+mongoose.connect(dbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
     if (err) {
-        console.log("error!! " + err)
+        console.log("Error: " + err);
     } else {
-      //  console.log("MongoDB Connection Successful")
+        console.log("MongoDB Connection Successful");
     }
-})
+});
 
 var Schema = mongoose.Schema;
 
